30f3645f85b6f8493aacc83f756b40bac1d28699
fulcrum <janis.civan@gmail.com>
2024-02-22 09:09:23 +0100

    remove large file from opencv

--- Mnemosy/Solution/Code/Dependencies/assimp-master/revision.h
+++ Mnemosy/Solution/Code/Dependencies/assimp-master/revision.h
@@ -1,7 +1,7 @@
 #ifndef ASSIMP_REVISION_H_INC
 #define ASSIMP_REVISION_H_INC
 
-#define GitVersion 0x9e8e4c85
+#define GitVersion 0x8412c531
 #define GitBranch "master"
 
 #define VER_MAJOR 5
@@ -16,7 +16,7 @@
 #if (GitVersion == 0)
 #define VER_FILEVERSION_STR         STR(VER_MAJOR) "." STR(VER_MINOR) "." STR(VER_PATCH) "." STR(VER_BUILD)
 #else
-#define VER_FILEVERSION_STR         STR(VER_MAJOR) "." STR(VER_MINOR) "." STR(VER_PATCH) "." STR(VER_BUILD) " (Commit 9e8e4c85)"
+#define VER_FILEVERSION_STR         STR(VER_MAJOR) "." STR(VER_MINOR) "." STR(VER_PATCH) "." STR(VER_BUILD) " (Commit 8412c531)"
 #endif
 #define VER_COPYRIGHT_STR           "\xA9 2006-2023"
 
