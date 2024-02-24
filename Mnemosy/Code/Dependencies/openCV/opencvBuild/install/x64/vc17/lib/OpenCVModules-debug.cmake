92aa35a1345ccab76f8319302901de2507034cb5
fulcrum <janis.civan@gmail.com>
2024-02-24 16:25:10 +0100

    fixed git ignore ignoring important files

---
+++ Mnemosy/Code/Dependencies/openCV/opencvBuild/install/x64/vc17/lib/OpenCVModules-debug.cmake
@@ -0,0 +1,159 @@
+#----------------------------------------------------------------
+# Generated CMake target import file for configuration "Debug".
+#----------------------------------------------------------------
+
+# Commands may need to know the format version.
+set(CMAKE_IMPORT_FILE_VERSION 1)
+
+# Import target "opencv_core" for configuration "Debug"
+set_property(TARGET opencv_core APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(opencv_core PROPERTIES
+  IMPORTED_IMPLIB_DEBUG "${_IMPORT_PREFIX}/x64/vc17/lib/opencv_core490d.lib"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/x64/vc17/bin/opencv_core490d.dll"
+  )
+
+list(APPEND _cmake_import_check_targets opencv_core )
+list(APPEND _cmake_import_check_files_for_opencv_core "${_IMPORT_PREFIX}/x64/vc17/lib/opencv_core490d.lib" "${_IMPORT_PREFIX}/x64/vc17/bin/opencv_core490d.dll" )
+
+# Import target "opencv_flann" for configuration "Debug"
+set_property(TARGET opencv_flann APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(opencv_flann PROPERTIES
+  IMPORTED_IMPLIB_DEBUG "${_IMPORT_PREFIX}/x64/vc17/lib/opencv_flann490d.lib"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/x64/vc17/bin/opencv_flann490d.dll"
+  )
+
+list(APPEND _cmake_import_check_targets opencv_flann )
+list(APPEND _cmake_import_check_files_for_opencv_flann "${_IMPORT_PREFIX}/x64/vc17/lib/opencv_flann490d.lib" "${_IMPORT_PREFIX}/x64/vc17/bin/opencv_flann490d.dll" )
+
+# Import target "opencv_imgproc" for configuration "Debug"
+set_property(TARGET opencv_imgproc APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(opencv_imgproc PROPERTIES
+  IMPORTED_IMPLIB_DEBUG "${_IMPORT_PREFIX}/x64/vc17/lib/opencv_imgproc490d.lib"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/x64/vc17/bin/opencv_imgproc490d.dll"
+  )
+
+list(APPEND _cmake_import_check_targets opencv_imgproc )
+list(APPEND _cmake_import_check_files_for_opencv_imgproc "${_IMPORT_PREFIX}/x64/vc17/lib/opencv_imgproc490d.lib" "${_IMPORT_PREFIX}/x64/vc17/bin/opencv_imgproc490d.dll" )
+
+# Import target "opencv_ml" for configuration "Debug"
+set_property(TARGET opencv_ml APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(opencv_ml PROPERTIES
+  IMPORTED_IMPLIB_DEBUG "${_IMPORT_PREFIX}/x64/vc17/lib/opencv_ml490d.lib"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/x64/vc17/bin/opencv_ml490d.dll"
+  )
+
+list(APPEND _cmake_import_check_targets opencv_ml )
+list(APPEND _cmake_import_check_files_for_opencv_ml "${_IMPORT_PREFIX}/x64/vc17/lib/opencv_ml490d.lib" "${_IMPORT_PREFIX}/x64/vc17/bin/opencv_ml490d.dll" )
+
+# Import target "opencv_photo" for configuration "Debug"
+set_property(TARGET opencv_photo APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(opencv_photo PROPERTIES
+  IMPORTED_IMPLIB_DEBUG "${_IMPORT_PREFIX}/x64/vc17/lib/opencv_photo490d.lib"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/x64/vc17/bin/opencv_photo490d.dll"
+  )
+
+list(APPEND _cmake_import_check_targets opencv_photo )
+list(APPEND _cmake_import_check_files_for_opencv_photo "${_IMPORT_PREFIX}/x64/vc17/lib/opencv_photo490d.lib" "${_IMPORT_PREFIX}/x64/vc17/bin/opencv_photo490d.dll" )
+
+# Import target "opencv_dnn" for configuration "Debug"
+set_property(TARGET opencv_dnn APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(opencv_dnn PROPERTIES
+  IMPORTED_IMPLIB_DEBUG "${_IMPORT_PREFIX}/x64/vc17/lib/opencv_dnn490d.lib"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/x64/vc17/bin/opencv_dnn490d.dll"
+  )
+
+list(APPEND _cmake_import_check_targets opencv_dnn )
+list(APPEND _cmake_import_check_files_for_opencv_dnn "${_IMPORT_PREFIX}/x64/vc17/lib/opencv_dnn490d.lib" "${_IMPORT_PREFIX}/x64/vc17/bin/opencv_dnn490d.dll" )
+
+# Import target "opencv_features2d" for configuration "Debug"
+set_property(TARGET opencv_features2d APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(opencv_features2d PROPERTIES
+  IMPORTED_IMPLIB_DEBUG "${_IMPORT_PREFIX}/x64/vc17/lib/opencv_features2d490d.lib"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/x64/vc17/bin/opencv_features2d490d.dll"
+  )
+
+list(APPEND _cmake_import_check_targets opencv_features2d )
+list(APPEND _cmake_import_check_files_for_opencv_features2d "${_IMPORT_PREFIX}/x64/vc17/lib/opencv_features2d490d.lib" "${_IMPORT_PREFIX}/x64/vc17/bin/opencv_features2d490d.dll" )
+
+# Import target "opencv_imgcodecs" for configuration "Debug"
+set_property(TARGET opencv_imgcodecs APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(opencv_imgcodecs PROPERTIES
+  IMPORTED_IMPLIB_DEBUG "${_IMPORT_PREFIX}/x64/vc17/lib/opencv_imgcodecs490d.lib"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/x64/vc17/bin/opencv_imgcodecs490d.dll"
+  )
+
+list(APPEND _cmake_import_check_targets opencv_imgcodecs )
+list(APPEND _cmake_import_check_files_for_opencv_imgcodecs "${_IMPORT_PREFIX}/x64/vc17/lib/opencv_imgcodecs490d.lib" "${_IMPORT_PREFIX}/x64/vc17/bin/opencv_imgcodecs490d.dll" )
+
+# Import target "opencv_videoio" for configuration "Debug"
+set_property(TARGET opencv_videoio APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(opencv_videoio PROPERTIES
+  IMPORTED_IMPLIB_DEBUG "${_IMPORT_PREFIX}/x64/vc17/lib/opencv_videoio490d.lib"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/x64/vc17/bin/opencv_videoio490d.dll"
+  )
+
+list(APPEND _cmake_import_check_targets opencv_videoio )
+list(APPEND _cmake_import_check_files_for_opencv_videoio "${_IMPORT_PREFIX}/x64/vc17/lib/opencv_videoio490d.lib" "${_IMPORT_PREFIX}/x64/vc17/bin/opencv_videoio490d.dll" )
+
+# Import target "opencv_calib3d" for configuration "Debug"
+set_property(TARGET opencv_calib3d APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(opencv_calib3d PROPERTIES
+  IMPORTED_IMPLIB_DEBUG "${_IMPORT_PREFIX}/x64/vc17/lib/opencv_calib3d490d.lib"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/x64/vc17/bin/opencv_calib3d490d.dll"
+  )
+
+list(APPEND _cmake_import_check_targets opencv_calib3d )
+list(APPEND _cmake_import_check_files_for_opencv_calib3d "${_IMPORT_PREFIX}/x64/vc17/lib/opencv_calib3d490d.lib" "${_IMPORT_PREFIX}/x64/vc17/bin/opencv_calib3d490d.dll" )
+
+# Import target "opencv_highgui" for configuration "Debug"
+set_property(TARGET opencv_highgui APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(opencv_highgui PROPERTIES
+  IMPORTED_IMPLIB_DEBUG "${_IMPORT_PREFIX}/x64/vc17/lib/opencv_highgui490d.lib"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/x64/vc17/bin/opencv_highgui490d.dll"
+  )
+
+list(APPEND _cmake_import_check_targets opencv_highgui )
+list(APPEND _cmake_import_check_files_for_opencv_highgui "${_IMPORT_PREFIX}/x64/vc17/lib/opencv_highgui490d.lib" "${_IMPORT_PREFIX}/x64/vc17/bin/opencv_highgui490d.dll" )
+
+# Import target "opencv_objdetect" for configuration "Debug"
+set_property(TARGET opencv_objdetect APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(opencv_objdetect PROPERTIES
+  IMPORTED_IMPLIB_DEBUG "${_IMPORT_PREFIX}/x64/vc17/lib/opencv_objdetect490d.lib"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/x64/vc17/bin/opencv_objdetect490d.dll"
+  )
+
+list(APPEND _cmake_import_check_targets opencv_objdetect )
+list(APPEND _cmake_import_check_files_for_opencv_objdetect "${_IMPORT_PREFIX}/x64/vc17/lib/opencv_objdetect490d.lib" "${_IMPORT_PREFIX}/x64/vc17/bin/opencv_objdetect490d.dll" )
+
+# Import target "opencv_stitching" for configuration "Debug"
+set_property(TARGET opencv_stitching APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(opencv_stitching PROPERTIES
+  IMPORTED_IMPLIB_DEBUG "${_IMPORT_PREFIX}/x64/vc17/lib/opencv_stitching490d.lib"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/x64/vc17/bin/opencv_stitching490d.dll"
+  )
+
+list(APPEND _cmake_import_check_targets opencv_stitching )
+list(APPEND _cmake_import_check_files_for_opencv_stitching "${_IMPORT_PREFIX}/x64/vc17/lib/opencv_stitching490d.lib" "${_IMPORT_PREFIX}/x64/vc17/bin/opencv_stitching490d.dll" )
+
+# Import target "opencv_video" for configuration "Debug"
+set_property(TARGET opencv_video APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(opencv_video PROPERTIES
+  IMPORTED_IMPLIB_DEBUG "${_IMPORT_PREFIX}/x64/vc17/lib/opencv_video490d.lib"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/x64/vc17/bin/opencv_video490d.dll"
+  )
+
+list(APPEND _cmake_import_check_targets opencv_video )
+list(APPEND _cmake_import_check_files_for_opencv_video "${_IMPORT_PREFIX}/x64/vc17/lib/opencv_video490d.lib" "${_IMPORT_PREFIX}/x64/vc17/bin/opencv_video490d.dll" )
+
+# Import target "opencv_gapi" for configuration "Debug"
+set_property(TARGET opencv_gapi APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(opencv_gapi PROPERTIES
+  IMPORTED_IMPLIB_DEBUG "${_IMPORT_PREFIX}/x64/vc17/lib/opencv_gapi490d.lib"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/x64/vc17/bin/opencv_gapi490d.dll"
+  )
+
+list(APPEND _cmake_import_check_targets opencv_gapi )
+list(APPEND _cmake_import_check_files_for_opencv_gapi "${_IMPORT_PREFIX}/x64/vc17/lib/opencv_gapi490d.lib" "${_IMPORT_PREFIX}/x64/vc17/bin/opencv_gapi490d.dll" )
+
+# Commands beyond this point should not need to know the version.
+set(CMAKE_IMPORT_FILE_VERSION)
