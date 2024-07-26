125e9f393d73230e8b510cba99c2059ecfb11a81
fulcrum <janis.civan@gmail.com>
2024-07-26 17:23:35 +0200

    moven quick selection into main menu bar

--- Mnemosy/Code/Application/Src/GuiPanels/MainMenuBarGuiPanel.cpp
+++ Mnemosy/Code/Application/Src/GuiPanels/MainMenuBarGuiPanel.cpp
@@ -16,7 +16,21 @@
 
 #include "Include/Systems/MaterialLibraryRegistry.h"
 
+
+#include "Include/Systems/SkyboxAssetRegistry.h"
+
+#include "Include/Graphics/Skybox.h"
+#include "Include/Graphics/RenderMesh.h"
+#include "Include/Graphics/Scene.h"
+#include "Include/Graphics/Renderer.h"
+
+
 #include "External/ImGui/imgui.h"
+
+
+
+
+
 
 namespace mnemosy::gui
 {
@@ -36,6 +50,124 @@ namespace mnemosy::gui
 			
 			DataDropdown();
 			ViewsDropdown();
+
+
+
+
+
+			MnemosyEngine& engine = MnemosyEngine::GetInstance();
+
+			graphics::Scene& scene = engine.GetScene();
+
+			ImGui::Spacing();
+			ImGui::Spacing();
+			ImGui::Spacing();
+			ImGui::Spacing();
+			ImGui::Text("														");
+
+			// Quick select mesh
+			{
+				graphics::RenderMesh& renderMesh = scene.GetMesh();
+
+				const char* previewMesh_List[7] = { "Custom","Default","Cube","Plane","Sphere","Cylinder","Suzanne" }; // they need to be ordered the same as in lightType Enum in light class
+
+				int previewMesh_Current = (int)scene.GetCurrentPreviewMesh();
+
+
+				ImGui::Text("Mesh: ");
+				ImGui::SetNextItemWidth(120.0f);
+				ImGui::Combo(" ##ViewportMesh", &previewMesh_Current, previewMesh_List, IM_ARRAYSIZE(previewMesh_List));
+
+				if ((int)scene.GetCurrentPreviewMesh() != previewMesh_Current)
+				{
+					if (previewMesh_Current == 0)
+						scene.SetPreviewMesh(graphics::PreviewMesh::Custom);
+					else if (previewMesh_Current == 1)
+						scene.SetPreviewMesh(graphics::PreviewMesh::Default);
+					else if (previewMesh_Current == 2)
+						scene.SetPreviewMesh(graphics::PreviewMesh::Cube);
+					else if (previewMesh_Current == 3)
+						scene.SetPreviewMesh(graphics::PreviewMesh::Plane);
+					else if (previewMesh_Current == 4)
+						scene.SetPreviewMesh(graphics::PreviewMesh::Sphere);
+					else if (previewMesh_Current == 5)
+						scene.SetPreviewMesh(graphics::PreviewMesh::Cylinder);
+					else if (previewMesh_Current == 6)
+						scene.SetPreviewMesh(graphics::PreviewMesh::Suzanne);
+				}
+
+			}
+			ImGui::Spacing();
+			ImGui::Spacing();
+
+			// Quick Select Skybox
+			{
+
+				graphics::Skybox& skybox = scene.GetSkybox();
+				mnemosy::systems::SkyboxAssetRegistry& skyboxRegistry = engine.GetSkyboxAssetRegistry();
+
+				// -- Skybox Selection Menu
+				bool assetsInRegistry = !skyboxRegistry.GetVectorOfNames().empty();
+				if (assetsInRegistry) // if there are no assets in the internal vector this will crash
+				{
+
+					int current = skyboxRegistry.GetCurrentSelected();
+
+					const char* combo_preview_value = skyboxRegistry.GetVectorOfNames()[current].c_str();
+					int previousSelected = current;
+
+					ImGui::Text("Skybox: ");
+					ImGui::SetNextItemWidth(200.0f);
+					if (ImGui::BeginCombo(" ##ViewportSkybox", combo_preview_value, 0))
+					{
+						for (int n = 0; n < skyboxRegistry.GetVectorOfNames().size(); n++)
+						{
+							const bool is_selected = (current == n);
+							if (ImGui::Selectable(skyboxRegistry.GetVectorOfNames()[n].c_str(), is_selected))
+								current = n;
+
+							// Set the initial focus when opening the combo (scrolling + keyboard navigation focus)
+							if (is_selected)
+								ImGui::SetItemDefaultFocus();
+						}
+						ImGui::EndCombo();
+					}
+
+					if (current != previousSelected)  // Selection has happend
+					{
+						skybox.LoadPreviewSkybox(skyboxRegistry.GetVectorOfNames()[current]);
+						engine.GetRenderer().SetShaderSkyboxUniforms(skybox);
+					}
+				}
+
+
+			}
+
+			ImGui::Spacing();
+			ImGui::Spacing();
+
+			// Rendering Selection
+			{
+
+				graphics::Renderer& renderer = engine.GetRenderer();
+
+				const char* renderModes_List[9] = { "Shaded","Albedo","Roughness","Metallic","Normal","AmbientOcclusion","Emissive","Height", "Opacity" }; // they need to be ordered the same as in RenderModes Enum in renderer class
+				int rendermode_Current = renderer.GetCurrentRenderModeInt();
+
+
+				ImGui::Text("Render: ");
+				ImGui::SetNextItemWidth(180.0f);
+				ImGui::Combo(" ##ViewportRenderModes", &rendermode_Current, renderModes_List, IM_ARRAYSIZE(renderModes_List));
+
+
+
+				if (rendermode_Current != renderer.GetCurrentRenderModeInt())
+				{
+					renderer.SetRenderMode((graphics::RenderModes)rendermode_Current);
+				}
+
+			}
+
 
 
 
--- Mnemosy/Code/Application/Src/GuiPanels/ViewportGuiPanel.cpp
+++ Mnemosy/Code/Application/Src/GuiPanels/ViewportGuiPanel.cpp
@@ -10,11 +10,6 @@
 #include "Include/Graphics/Scene.h"
 #include "Include/Graphics/Renderer.h"
 
-//#include "Include/Core/FileDirectories.h"
-
-#ifdef MNEMOSY_PLATFORM_WINDOWS
-#include "Include/Core/Utils/PlatfromUtils_Windows.h"
-#endif // MNEMOSY_PLATFORM_WINDOWS
 
 
 namespace mnemosy::gui
@@ -35,121 +30,8 @@ namespace mnemosy::gui
 
 
 
-		ImGui::Begin(panelName, &showPanel, ImGuiWindowFlags_MenuBar);
-
-
-		//ImGui::BeginMenuBar();
-		if (ImGui::BeginMenuBar()) {
-
-			MnemosyEngine& engine = MnemosyEngine::GetInstance();
-
-			graphics::Scene& scene = engine.GetScene();
-		
-
-			// Quick select mesh
-			{
-				graphics::RenderMesh& renderMesh = scene.GetMesh();
-
-				const char* previewMesh_List[7] = { "Custom","Default","Cube","Plane","Sphere","Cylinder","Suzanne" }; // they need to be ordered the same as in lightType Enum in light class
-
-				int previewMesh_Current = (int)scene.GetCurrentPreviewMesh();
-
-
-				ImGui::Text("Mesh: ");
-				ImGui::SetNextItemWidth(120.0f);
-				ImGui::Combo(" ##ViewportMesh", &previewMesh_Current, previewMesh_List, IM_ARRAYSIZE(previewMesh_List));
-
-				if ((int)scene.GetCurrentPreviewMesh() != previewMesh_Current)
-				{
-					if (previewMesh_Current == 0)
-						scene.SetPreviewMesh(graphics::PreviewMesh::Custom);
-					else if (previewMesh_Current == 1)
-						scene.SetPreviewMesh(graphics::PreviewMesh::Default);
-					else if (previewMesh_Current == 2)
-						scene.SetPreviewMesh(graphics::PreviewMesh::Cube);
-					else if (previewMesh_Current == 3)
-						scene.SetPreviewMesh(graphics::PreviewMesh::Plane);
-					else if (previewMesh_Current == 4)
-						scene.SetPreviewMesh(graphics::PreviewMesh::Sphere);
-					else if (previewMesh_Current == 5)
-						scene.SetPreviewMesh(graphics::PreviewMesh::Cylinder);
-					else if (previewMesh_Current == 6)
-						scene.SetPreviewMesh(graphics::PreviewMesh::Suzanne);
-				}
-
-			}
-
-			// Quick Select Skybox
-			{
-
-				graphics::Skybox& skybox = scene.GetSkybox();
-				mnemosy::systems::SkyboxAssetRegistry& skyboxRegistry = engine.GetSkyboxAssetRegistry();
-
-				// -- Skybox Selection Menu
-				bool assetsInRegistry = !skyboxRegistry.GetVectorOfNames().empty();
-				if (assetsInRegistry) // if there are no assets in the internal vector this will crash
-				{
-
-					int current = skyboxRegistry.GetCurrentSelected();
-
-					const char* combo_preview_value = skyboxRegistry.GetVectorOfNames()[current].c_str();
-					int previousSelected = current;
-
-					ImGui::Text("Skybox: ");
-					ImGui::SetNextItemWidth(200.0f);
-					if (ImGui::BeginCombo(" ##ViewportSkybox", combo_preview_value, 0))
-					{
-						for (int n = 0; n < skyboxRegistry.GetVectorOfNames().size(); n++)
-						{
-							const bool is_selected = (current == n);
-							if (ImGui::Selectable(skyboxRegistry.GetVectorOfNames()[n].c_str(), is_selected))
-								current = n;
-
-							// Set the initial focus when opening the combo (scrolling + keyboard navigation focus)
-							if (is_selected)
-								ImGui::SetItemDefaultFocus();
-						}
-						ImGui::EndCombo();
-					}
-
-					if (current != previousSelected)  // Selection has happend
-					{
-						skybox.LoadPreviewSkybox(skyboxRegistry.GetVectorOfNames()[current]);
-						engine.GetRenderer().SetShaderSkyboxUniforms(skybox);
-					}
-				}
-
-
-			}
-
-			// Rendering Selection
-			{
-
-				graphics::Renderer& renderer = engine.GetRenderer();
-
-				const char* renderModes_List[9] = { "Shaded","Albedo","Roughness","Metallic","Normal","AmbientOcclusion","Emissive","Height", "Opacity"}; // they need to be ordered the same as in RenderModes Enum in renderer class
-				int rendermode_Current = renderer.GetCurrentRenderModeInt();
-
-
-				ImGui::Text("Render: ");
-				ImGui::SetNextItemWidth(180.0f);
-				ImGui::Combo(" ##ViewportRenderModes", &rendermode_Current, renderModes_List, IM_ARRAYSIZE(renderModes_List));
-
-
-
-				if (rendermode_Current != renderer.GetCurrentRenderModeInt())
-				{
-					renderer.SetRenderMode((graphics::RenderModes)rendermode_Current);
-				}
-
-			}
-
-
-
-			ImGui::EndMenuBar();
-		}
-
-
+		ImGui::Begin(panelName, &showPanel);
+	
 
 		DrawViewport();
 
